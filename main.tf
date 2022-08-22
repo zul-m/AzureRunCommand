aa66c6167c7e619ecbaa01f08c3af2c2d04ffab9
Zulfikar Muhamad <zul.m@icloud.com>
2022-08-22 15:36:59 +0800

    Modified for easier commits going forward

--- main.tf
+++ main.tf
@@ -15,45 +15,27 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-
-}
-
 locals {
-  resource_group_name    = var.infrastructure_id
-  location               = var.location
-  azurerm_key_vault_name = "${var.infrastructure_id}kvt"
-  admin_username         = random_id.random.id
-  admin_password         = random_password.password.result
-  vnet_name              = "${var.infrastructure_id}vnet"
-  vm_nic_name            = "${var.infrastructure_id}nic"
-  vm_name                = "${var.infrastructure_id}vm"
-  vm_public_ip_name      = "${var.infrastructure_id}ip"
-  vm_nsg_name            = "${var.infrastructure_id}nsg"
+  resource_group_name = var.infrastructure_id
+  location            = var.location
 }
-
-resource "random_id" "random" {
-  byte_length = 6
-}
-
-resource "random_password" "password" {
-  length  = 16
-  special = true
-}
-
-data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
   location = local.location
 }
 
+# Using values from resource "azurerm_resource_group" instead of local.resource_group_name and local.location
+# To avoid the "Resource group could not be found." error
+module "secrets" {
+  source              = "./modules/secrets"
+  infrastructure_id   = var.infrastructure_id
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = local.vnet_name
+  name                = module.secrets.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -69,7 +51,7 @@ resource "azurerm_subnet" "vm_subnet" {
 }
 
 resource "azurerm_network_interface" "vm_nic" {
-  name                = local.vm_nic_name
+  name                = module.secrets.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -82,12 +64,12 @@ resource "azurerm_network_interface" "vm_nic" {
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = local.vm_name
+  name                = module.secrets.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_E4s_v5"
-  admin_username      = local.admin_username
-  admin_password      = local.admin_password
+  admin_username      = module.secrets.admin_username
+  admin_password      = module.secrets.admin_password
   network_interface_ids = [
     azurerm_network_interface.vm_nic.id
   ]
@@ -110,14 +92,14 @@ resource "azurerm_windows_virtual_machine" "vm" {
 }
 
 resource "azurerm_public_ip" "vm_public_ip" {
-  name                = local.vm_public_ip_name
+  name                = module.secrets.vm_public_ip_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
 }
 
 resource "azurerm_network_security_group" "vm_nsg" {
-  name                = local.vm_nsg_name
+  name                = module.secrets.vm_nsg_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -173,75 +155,4 @@ resource "azurerm_network_security_group" "vm_nsg" {
 resource "azurerm_subnet_network_security_group_association" "vm_nsg_associate" {
   subnet_id                 = azurerm_subnet.vm_subnet.id
   network_security_group_id = azurerm_network_security_group.vm_nsg.id
-}
-
-resource "azurerm_key_vault" "keyvault" {
-  name                        = local.azurerm_key_vault_name
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get", "List",
-    ]
-
-    secret_permissions = [
-      "Get", "List", "Set", "Delete", "Purge", "Recover",
-    ]
-
-    storage_permissions = [
-      "Get", "List",
-    ]
-  }
-}
-
-resource "azurerm_key_vault_secret" "username" {
-  name         = "admin-username"
-  value        = local.admin_username
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
-resource "azurerm_key_vault_secret" "password" {
-  name         = "admin-password"
-  value        = local.admin_password
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
-resource "azurerm_key_vault_secret" "vnet" {
-  name         = "vnet-name"
-  value        = local.vnet_name
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
-resource "azurerm_key_vault_secret" "nic" {
-  name         = "vm-nic-name"
-  value        = local.vm_nic_name
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
-resource "azurerm_key_vault_secret" "vm" {
-  name         = "vm-name"
-  value        = local.vm_name
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
-resource "azurerm_key_vault_secret" "public_ip" {
-  name         = "vm-public-ip-name"
-  value        = local.vm_public_ip_name
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
-resource "azurerm_key_vault_secret" "nsg" {
-  name         = "vm-nsg-name"
-  value        = local.vm_nsg_name
-  key_vault_id = azurerm_key_vault.keyvault.id
 }
